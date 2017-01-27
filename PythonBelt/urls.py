c060c1dec906050916fb4db5755b350e01486928
RossHargrove2856 <rhargrove05@hotmail.com>
2017-01-27 09:11:03 -0600

    changed home page

--- PythonBelt/urls.py
+++ PythonBelt/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.login_and_reg.urls')),
-    url(r'^', include('apps.myBelt.urls'))
+    url(r'^belt', include('apps.myBelt.urls'))
 ]
